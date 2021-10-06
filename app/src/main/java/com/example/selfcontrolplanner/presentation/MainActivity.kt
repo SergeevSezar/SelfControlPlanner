7836e11f3017858dc68fd430ce363add8abd2ec8
SergeevSezar <sergeev.sezar@yandex.ru>
2021-10-07 06:35:12 +0900

    add swipe listener and refactor functions

--- app/src/main/java/com/example/selfcontrolplanner/presentation/MainActivity.kt
+++ app/src/main/java/com/example/selfcontrolplanner/presentation/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.selfcontrolplanner.R
-import com.example.selfcontrolplanner.domain.PlannerItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,11 +35,42 @@ class MainActivity : AppCompatActivity() {
             PlanListAdapter.VIEW_TYPE_DISABLED,
             PlanListAdapter.MAX_POOL_SIZE
         )
-        adapter.onPlanItemLongClickListener = {
-            viewModel.editPlannerList(it)
+        setupLongClickListener()
+        setupClickListener()
+        setupSwipeListener(rvPlanList)
+    }
+
+    private fun setupSwipeListener(rvPlanList: RecyclerView?) {
+        val callback = object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = adapter.planList[viewHolder.adapterPosition]
+                viewModel.removePlannerItem(item)
+            }
         }
+        val itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper.attachToRecyclerView(rvPlanList)
+    }
+
+    private fun setupClickListener() {
         adapter.onPlanItemClickListener = {
             Log.d("MainActivity", it.toString())
+        }
+    }
+
+    private fun setupLongClickListener() {
+        adapter.onPlanItemLongClickListener = {
+            viewModel.editPlannerList(it)
         }
     }
 }
